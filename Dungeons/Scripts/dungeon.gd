758e02b015c3edd9c12ec1d2ab3e0923b103f36a
Arturo Jiménez de los Galanes Reguillos <arturo.jimenezdelosgalanes@outsourcing.moeveglobal.com>
2026-07-31 20:54:20 +0200

    Add tactile support

--- Dungeons/Scripts/dungeon.gd
+++ Dungeons/Scripts/dungeon.gd
@@ -4,3 +4,7 @@ class_name Dungeon extends Node2D
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		LevelManager.exit_dungeon() 
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_GO_BACK_REQUEST || what == NOTIFICATION_WM_CLOSE_REQUEST:
+		LevelManager.exit_dungeon()
